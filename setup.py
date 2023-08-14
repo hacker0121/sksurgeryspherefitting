4c39aab69cee715ca8e002fd891dbd372a986a9f
liyangang <3201188794@qq.com>
2023-08-14 13:30:17 +0800

    updated setup.py and readme (#2)

--- setup.py
+++ setup.py
@@ -14,12 +14,15 @@ setup(
     name='scikit-surgery-sphere-fitting',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description='scikit-surgery-sphere-fitting implements a least squares sphere fitting algorithm, to read a vtk poly data file, a config file, and outputs the fitted sphere.',
+    description='scikit-surgery-sphere-fitting implements a
+                  least squares sphere fitting algorithm,
+                  to read a vtk poly data file, a config file, and
+                  outputs the fitted sphere',
     long_description=long_description,
     long_description_content_type='text/x-rst',
-    url='https://github.com/mxochicale/sksurgeryspherefitting',
-    author='Miguel Xochicale',
-    author_email='YOUR-EMAIL@ucl.ac.uk',
+    url='https://github.com/StephenThompson/sksurgeryspherefitting',
+    author='Stephen Thompson',
+    author_email='s.thompson@ucl.ac.uk',
     license='BSD-3 license',
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -34,7 +37,6 @@ setup(
 
 
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
 
         'Topic :: Scientific/Engineering :: Information Analysis',
@@ -47,11 +49,15 @@ setup(
         exclude=[
             'doc',
             'tests',
+            'data'
         ]
     ),
 
     install_requires=[
-        'numpy',
+        'numpy>=1.11',
+        'spicy',
+        'vtk>=9.2.6',
+        'scikit-surgeryvtk>=2.0.1'
     ],
 
     entry_points={
